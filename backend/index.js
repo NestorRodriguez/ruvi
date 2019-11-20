ea8705b53f4282ca2e6ea1394722d8189933d991
luis <laforero1@misena.edu.co>
2019-11-19 21:11:20 -0500

    5 servicios en backend

--- backend/index.js
+++ backend/index.js
@@ -44,11 +44,15 @@ app.get('/', function(req, res) {
  **************************************************/
 
 
-// MP_niveles_educacion
 app.get('/', function(req, res) {
     console.log('Página de Inicio ');
-    res.send("Bienvenidos al servidor <strong> TuMap </strong>");
+    res.send("Bienvenidos al servidor <strong> ruvi </strong>");
 });
+/***************************
+MP_niveles_educacion
+Crear datos niveles de educación
+***************************/
+
 
 app.get('/ruvi/niveles_educacion', (req, res) => {
     console.log('Consultar datos de los niveles de educacion');
@@ -129,8 +133,12 @@ app.delete('/ruvi/niveles_educacion/:id_niveledu', (req, res) => {
     });
 });
 
+/********************
+ *  
+ MP_guardar_registro
+ * 
+ ********************/
 
-// MP_guardar_registro
 app.get('/ruvi/guardar_registro', (req, res) => {
     console.log('Consultar datos guardar los registros');
     var query = db.query('select * from guardar_registro', (error, result) => {
@@ -241,8 +249,12 @@ app.delete('/ruvi/guardar_registro/:id_guardar_registro', (req, res) => {
         }
     });
 });
+/*******************
+ *
+ MP_nucleo_familiar 
+ * 
+ ******************/
 
-// MP_nucleo_familiar
 app.get('/ruvi/nucleo_familiar', (req, res) => {
     console.log('Consultar datos del  nucleo familiar');
     var query = db.query('select * from nucleo_familiar', (error, result) => {
@@ -322,8 +334,12 @@ app.delete('/ruvi/nucleo_familiar/:id_nucleofam', (req, res) => {
     });
 });
 
+/**********
+ * 
+ MP_roles
+ *
+ **********/
 
-// MP_roles
 app.get('/ruvi/roles', (req, res) => {
     console.log('Consultar datos de los roles');
     var query = db.query('select * from roles', (error, result) => {
@@ -356,7 +372,7 @@ app.get('/ruvi/roles/:id_rol', (req, res) => {
             }
         });
     })
-    // crear
+    // 
 app.post('/ruvi/roles', (req, res) => {
     const dato = req.body
 
@@ -404,10 +420,15 @@ app.delete('/ruvi/roles/:id_rol', (req, res) => {
     });
 });
 
-// MP_roles
-app.get('/ruvi/roles', (req, res) => {
-    console.log('Consultar datos de los roles');
-    var query = db.query('select * from roles', (error, result) => {
+/*****
+ * 
+ MP_vivienda 
+ * 
+ ****/
+
+app.get('/ruvi/vivienda', (req, res) => {
+    console.log('Consultar datos de la vivienda');
+    var query = db.query('select * from vivienda', (error, result) => {
         try {
             if (error) {
                 throw error;
@@ -420,9 +441,9 @@ app.get('/ruvi/roles', (req, res) => {
         }
     });
 })
-app.get('/ruvi/roles/:id_rol', (req, res) => {
-        const id_rol = req.params.id_rol;
-        const sql = `SELECT * FROM roles WHERE id_rol='${id_rol}';`;
+app.get('/ruvi/vivienda/:id_viviendaper', (req, res) => {
+        const id_viviendaper = req.params.id_viviendaper;
+        const sql = `SELECT * FROM vivienda WHERE id_viviendaper='${id_viviendaper}';`;
         const query = db.query(sql, (error, result) => {
             try {
                 if (error) {
@@ -438,10 +459,10 @@ app.get('/ruvi/roles/:id_rol', (req, res) => {
         });
     })
     // crear
-app.post('/ruvi/roles', (req, res) => {
+app.post('/ruvi/vivienda', (req, res) => {
     const dato = req.body
 
-    const sql = `INSERT INTO roles (descripcion)
+    const sql = `INSERT INTO vivienda (descripcion)
   values ('${dato.descripcion}')`;
 
     db.query(sql, (error, result) => {
@@ -452,12 +473,12 @@ app.post('/ruvi/roles', (req, res) => {
         }
     });
 })
-app.put('/ruvi/roles', (req, res) => {
+app.put('/ruvi/vivienda', (req, res) => {
 
     const dato = req.body
 
-    const sql = `UPDATE roles SET id_rol= '${dato.descripcion}'
-          WHERE id_rol= '${dato.descripcion}';`;
+    const sql = `UPDATE vivienda SET id_viviendaper= '${dato.descripcion}'
+          WHERE id_viviendaper= '${dato.descripcion}';`;
 
     console.log(sql);
 
@@ -469,9 +490,9 @@ app.put('/ruvi/roles', (req, res) => {
         }
     });
 })
-app.delete('/ruvi/roles/:id_rol', (req, res) => {
-    const id_rol = req.params.id_rol;
-    const sql = `DELETE FROM roles WHERE id_rol = '${id_rol}';`;
+app.delete('/ruvi/vivienda/:id_viviendaper', (req, res) => {
+    const id_viviendaper = req.params.id_viviendaper;
+    const sql = `DELETE FROM vivienda WHERE id_viviendaper = '${id_viviendaper}';`;
     const query = db.query(sql, (error, result) => {
         try {
             if (error) {
@@ -484,6 +505,454 @@ app.delete('/ruvi/roles/:id_rol', (req, res) => {
         }
     });
 });
+
+/*****
+ * 
+ MP_usuario 
+ * 
+ ****/
+
+app.get('/ruvi/usuario', (req, res) => {
+    console.log('Consultar datos de  los usuario');
+    var query = db.query('select * from usuario', (error, result) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                console.log(result);
+                res.json(result)
+            }
+        } catch (error) {
+            res.json({ error: error.message })
+        }
+    });
+})
+app.get('/ruvi/usuario/:id_datos', (req, res) => {
+        const id_datos = req.params.id_datos;
+        const sql = `SELECT * FROM usuario WHERE id_datos='${id_datos}';`;
+        const query = db.query(sql, (error, result) => {
+            try {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log(result);
+                    const [data] = result;
+                    res.json(data)
+                }
+            } catch (error) {
+                res.json({ error: error.message })
+            }
+        });
+    })
+    //Insertar datos 
+app.post('/ruvi/usuario', (req, res) => {
+    const dato = {
+        nombres: req.body.nombres,
+        apellidos: req.body.apellidos,
+        edad: req.body.edad,
+        sexo: req.body.sexo,
+        direccion: req.body.direccion,
+        telefono: req.body.telefono,
+        correo: req.body.correo,
+        discapacidad: req.body.discapacidad,
+        desplazado: req.body.desplazado,
+    };
+
+    const sql = `INSERT INTO usuario SET nombres='${dato.nombres}',
+                                                  apellidos='${dato.apellidos}',
+                                                  edad='${dato.edad}',
+                                                  sexo='${dato.sexo}',
+                                                  direccion='${dato.direccion}',
+                                                  telefono='${dato.telefono}',
+                                                  correo='${dato.correo}',
+                                                  discapacidad='${dato.discapacidad}',
+                                                  desplazado='${dato.desplazado}',`;
+
+    db.query(sql, (error, result) => {
+        if (error) {
+            res.json({ error: error })
+        } else {
+            res.json(result)
+        }
+    });
+})
+
+//Actualizar
+app.put('/ruvi/usuario:id_datos', (req, res) => {
+    const id_datos = req.params.id_datos;
+
+    const dato = {
+        nombres: req.body.nombres,
+        apellidos: req.body.apellidos,
+        edad: req.body.edad,
+        sexo: req.body.sexo,
+        direccion: req.body.direccion,
+        telefono: req.body.telefono,
+        correo: req.body.correo,
+        discapacidad: req.body.discapacidad,
+        desplazado: req.body.desplazado,
+    };
+    let sets = [];
+    for (i in dato) {
+        if (dato[i] || dato[i] == 0) {
+            sets.push(`${i}='${dato[i]}'`);
+        }
+    }
+
+    const sql = `UPDATE usuario SET ${sets.join(', ')} WHERE id_datos='${id_datos}';`;
+    console.log(sql);
+
+    db.query(sql, (error, result) => {
+        if (error) {
+            res.json({ error: error })
+        } else {
+            res.json(result)
+        }
+    });
+})
+app.delete('/ruvi/usuario/:id_datos', (req, res) => {
+    const id_datos = req.params.id_datos;
+    const sql = `DELETE FROM usuario WHERE id_datos = '${id_datos}';`;
+    const query = db.query(sql, (error, result) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                res.json(result)
+            }
+        } catch (error) {
+            res.json({ error: error.message })
+        }
+    });
+});
+
+/*****
+ * 
+ MP_registro_documento 
+ * 
+ ****/
+
+app.get('/ruvi/registro_documento', (req, res) => {
+    console.log('Consultar datos de  los documentos');
+    var query = db.query('select * from registro_documento', (error, result) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                console.log(result);
+                res.json(result)
+            }
+        } catch (error) {
+            res.json({ error: error.message })
+        }
+    });
+})
+app.get('/ruvi/registro_documento/:id_registrodoc', (req, res) => {
+        const id_registrodoc = req.params.id_registrodoc;
+        const sql = `SELECT * FROM registro_documento WHERE id_registrodoc='${id_registrodoc}';`;
+        const query = db.query(sql, (error, result) => {
+            try {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log(result);
+                    const [data] = result;
+                    res.json(data)
+                }
+            } catch (error) {
+                res.json({ error: error.message })
+            }
+        });
+    })
+    //Insertar datos 
+app.post('/ruvi/registro_documento', (req, res) => {
+    const dato = {
+        tipo_documento: req.body.tipo_documento,
+        numero_documento: req.body.numero_documento,
+        registro_rivi: req.body.registro_rivi,
+    };
+
+    const sql = `INSERT INTO registro_documento SET tipo_documento='${dato.tipo_documento}',
+                                                  numero_documento='${dato.numero_documento}',
+                                                  registro_rivi='${dato.registro_rivi}',
+                                                  `;
+
+    db.query(sql, (error, result) => {
+        if (error) {
+            res.json({ error: error })
+        } else {
+            res.json(result)
+        }
+    });
+})
+
+//Actualizar
+app.put('/ruvi/registro_documento:id_registrodoc', (req, res) => {
+    const id_registrodoc = req.params.id_registrodoc;
+
+    const dato = {
+        tipo_documento: req.body.tipo_documento,
+        numero_documento: req.body.numero_documento,
+        registro_rivi: req.body.registro_rivi,
+
+    };
+    let sets = [];
+    for (i in dato) {
+        if (dato[i] || dato[i] == 0) {
+            sets.push(`${i}='${dato[i]}'`);
+        }
+    }
+
+    const sql = `UPDATE registro_documento SET ${sets.join(', ')} WHERE id_registrodoc='${id_registrodoc}';`;
+    console.log(sql);
+
+    db.query(sql, (error, result) => {
+        if (error) {
+            res.json({ error: error })
+        } else {
+            res.json(result)
+        }
+    });
+})
+app.delete('/ruvi/registro_documento/:id_registrodoc', (req, res) => {
+    const id_registrodoc = req.params.id_registrodoc;
+    const sql = `DELETE FROM registro_documento WHERE id_registrodoc = '${id_registrodoc}';`;
+    const query = db.query(sql, (error, result) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                res.json(result)
+            }
+        } catch (error) {
+            res.json({ error: error.message })
+        }
+    });
+});
+
+/*****
+ * 
+ MP_registro_usuarios 
+ * 
+ ****/
+
+app.get('/ruvi/registro_usuarios', (req, res) => {
+    console.log('Consultar datos de  los usuarios');
+    var query = db.query('select * from registro_usuarios', (error, result) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                console.log(result);
+                res.json(result)
+            }
+        } catch (error) {
+            res.json({ error: error.message })
+        }
+    });
+})
+app.get('/ruvi/registro_usuarios/:id_regisusu', (req, res) => {
+        const id_regisusu = req.params.id_regisusu;
+        const sql = `SELECT * FROM registro_usuarios WHERE id_regisusu='${id_regisusu}';`;
+        const query = db.query(sql, (error, result) => {
+            try {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log(result);
+                    const [data] = result;
+                    res.json(data)
+                }
+            } catch (error) {
+                res.json({ error: error.message })
+            }
+        });
+    })
+    //Insertar datos 
+app.post('/ruvi/registro_usuarios', (req, res) => {
+    const dato = {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        edad: req.body.edad,
+        sexo: req.body.sexo,
+        telefono: req.body.telefono,
+        correo: req.body.correo,
+        usuario: req.body.usuario,
+        contraseña: req.body.contraseña,
+        id_rol: req.body.id_rol,
+
+    };
+
+    const sql = `INSERT INTO registro_usuarios SET nombre='${dato.nombre}',
+                                                  apellido='${dato.apellido}',
+                                                  edad='${dato.edad}',
+                                                  sexo='${dato.sexo}',
+                                                  telefono='${dato.telefono}',
+                                                  correo='${dato.correo}',
+                                                  usuario='${dato.usuario}',
+                                                  contraseña='${dato.contraseña}',
+                                                  id_rol='${dato.id_rol}',
+                                                  `;
+
+    db.query(sql, (error, result) => {
+        if (error) {
+            res.json({ error: error })
+        } else {
+            res.json(result)
+        }
+    });
+})
+
+//Actualizar
+app.put('/ruvi/registro_usuarios:id_registrousu', (req, res) => {
+    const id_registrousu = req.params.id_registrousu;
+
+    const dato = {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        edad: req.body.edad,
+        sexo: req.body.sexo,
+        telefono: req.body.telefono,
+        correo: req.body.correo,
+        usuario: req.body.usuario,
+        contraseña: req.body.contraseña,
+        id_rol: req.body.id_rol,
+
+    };
+    let sets = [];
+    for (i in dato) {
+        if (dato[i] || dato[i] == 0) {
+            sets.push(`${i}='${dato[i]}'`);
+        }
+    }
+
+    const sql = `UPDATE registro_usuarios SET ${sets.join(', ')} WHERE id_registrousu='${id_registrousu}';`;
+    console.log(sql);
+
+    db.query(sql, (error, result) => {
+        if (error) {
+            res.json({ error: error })
+        } else {
+            res.json(result)
+        }
+    });
+})
+app.delete('/ruvi/registro_usuarios/:id_registrousu', (req, res) => {
+    const id_registrousu = req.params.id_registrousu;
+    const sql = `DELETE FROM registro_usuarios WHERE id_registrousu = '${id_registrousu}';`;
+    const query = db.query(sql, (error, result) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                res.json(result)
+            }
+        } catch (error) {
+            res.json({ error: error.message })
+        }
+    });
+});
+
+/*****
+ * 
+ MP_salud 
+ * 
+ ****/
+
+app.get('/ruvi/salud', (req, res) => {
+    console.log('Consultar datos de la salud');
+    var query = db.query('select * from salud', (error, result) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                console.log(result);
+                res.json(result)
+            }
+        } catch (error) {
+            res.json({ error: error.message })
+        }
+    });
+})
+app.get('/ruvi/salud/:id_saludper', (req, res) => {
+        const id_saludper = req.params.id_saludper;
+        const sql = `SELECT * FROM salud WHERE id_saludper='${id_saludper}';`;
+        const query = db.query(sql, (error, result) => {
+            try {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log(result);
+                    const [data] = result;
+                    res.json(data)
+                }
+            } catch (error) {
+                res.json({ error: error.message })
+            }
+        });
+    })
+    //Insertar datos 
+app.post('/ruvi/salud', (req, res) => {
+    const dato = {
+        nombre_eps: req.body.nombre_eps,
+        descripcion: req.body.descripcion,
+    };
+
+    const sql = `INSERT INTO salud SET nombre_eps='${dato.nombre_eps}',
+                                                  descripcion='${dato.descripcion}',`;
+
+    db.query(sql, (error, result) => {
+        if (error) {
+            res.json({ error: error })
+        } else {
+            res.json(result)
+        }
+    });
+})
+
+//Actualizar
+app.put('/ruvi/salud:id_saludper', (req, res) => {
+    const id_saludper = req.params.id_saludper;
+
+    const dato = {
+        nombre_eps: req.body.nombre_eps,
+        descripcion: req.body.descripcion,
+    };
+    let sets = [];
+    for (i in dato) {
+        if (dato[i] || dato[i] == 0) {
+            sets.push(`${i}='${dato[i]}'`);
+        }
+    }
+
+    const sql = `UPDATE salud SET ${sets.join(', ')} WHERE id_saludper='${id_saludper}';`;
+    console.log(sql);
+
+    db.query(sql, (error, result) => {
+        if (error) {
+            res.json({ error: error })
+        } else {
+            res.json(result)
+        }
+    });
+})
+app.delete('/ruvi/salud/:id_saludper', (req, res) => {
+    const id_saludper = req.params.id_saludper;
+    const sql = `DELETE FROM salud WHERE id_saludper = '${id_saludper}';`;
+    const query = db.query(sql, (error, result) => {
+        try {
+            if (error) {
+                throw error;
+            } else {
+                res.json(result)
+            }
+        } catch (error) {
+            res.json({ error: error.message })
+        }
+    });
+});
+
+
 
 app.listen(PORT, function() {
     console.log(`Server running at port ${PORT}`);
